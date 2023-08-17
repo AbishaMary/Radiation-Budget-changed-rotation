ac9145e69c1ad5d17abad9dfdd9eb77b69c56538
Abisha <abishagraj@gmail.com>
2023-08-17 15:04:51 +0200

    check

--- src/main_project.py
+++ src/main_project.py
@@ -1,1 +1,8 @@
+# %%
 import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+# %%
+control_run = xr.open_dataset('/work/mh0066/m300909/mpiesm-landveg/mpiesm-landveg/experiments/aquaplanet_sponge_layer_rotation_1x/outdata/aquaplanet_sponge_layer_rotation_1x_echam6_ATM_1976_2015.nc',decode_times=True)
+# %%
